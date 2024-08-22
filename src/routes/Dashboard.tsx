8a63b000341842937110e8066bdeafeb34c6435b
codepath <118053614+javier1234567890qw@users.noreply.github.com>
2024-08-22 15:43:11 +0200

    gh pages agg

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom"
 import { Inicio } from "../components/Inicio/Inicio"
-import { ServicioPant } from "../components/Inicio/ServicioPant"
 
 export const Dashboard = () => {
   return (
